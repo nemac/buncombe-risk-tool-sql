1115c359bd55598d1edee547a07715c4ec1a6a98
William Clark <wfclark2@gmail.com>
2016-07-25 17:01:18 -0400

    Update watersheds.sql

--- watersheds.sql
+++ watersheds.sql
@@ -19,3 +19,17 @@ a.geom
 from buncombe_watersheds as a, nlcd_change_bunc_vw as b
 where st_intersects(b.geom, a.geom)
 group by a.gid;
+
+alter table buncombe_watersheds 
+add column total_amount double precision,
+add column total_change double precision 
+
+update buncombe_watersheds as a 
+set total_amount = b.proportions
+from nlcd_bunc_proportions as b
+where a.gid = b.gid
+
+update buncombe_watersheds as a 
+set total_change = b.proportions
+from nlcd_change_bunc_proportions as b
+where a.gid = b.gid
