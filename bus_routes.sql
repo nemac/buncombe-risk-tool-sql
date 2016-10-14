fe599c6dd2cb5e2e07acc75e19527b169a5d0f98
William Clark <wfclark2@gmail.com>
2016-10-14 15:00:39 -0400

    Update bus_routes.sql

--- bus_routes.sql
+++ bus_routes.sql
@@ -1,41 +1,33 @@
-drop view bus_routes_fld;
-drop view bus_routes_ls;
 
-create or replace view bus_routes_wo_overpass as 
-select * from bus_routes 
+create or replace view bus_routes_count_ls_cbg as 
+select count(a.geom), a.geo_id, b.geom 
+from bus_routes_ls as a
+join coa_census_block_groups as b
+on a.geo_id = b.geo_id
 where route_id is null
+group by a.geo_id, b.geom;
 
-create or replace view bus_routes_fld as 
-select a.gid as gid, route_numb, geo_id, a.geom as geom
-from bus_routes as a 
-join fl5yr as b 
-on st_intersects(a.geom, b.geom)
-group by a.gid, a.geom;
-
-create or replace view bus_routes_ls as 
-select a.gid as gid, route_numb, geo_id, a.geom as geom
-from bus_routes as a 
-join debris_flow as b 
-on st_intersects(a.geom, b.geom)
-group by a.gid, a.geom;
+create or replace view bus_routes_count_fld_cbg as 
+select count(a.geom), a.geo_id, b.geom 
+from bus_routes_fld as a
+join coa_census_block_groups as b
+on a.geo_id = b.geo_id
+where route_id is null
+group by a.geo_id, b.geom;
 
 create or replace view bus_routes_fld_cbg as 
 select sum(st_length(a.geom::geography)) * .0006 as flooded_miles, a.geo_id, b.geom 
 from bus_routes_fld as a
 join coa_census_block_groups as b
 on a.geo_id = b.geo_id
-group by a.geo_id, b.geom
+where route_id is null
+group by a.geo_id, b.geom;
 
 create or replace view bus_routes_ls_cbg as 
 select sum(st_length(a.geom::geography)) * .0006 as flow_path_miles, a.geo_id, b.geom 
 from bus_routes_ls as a
 join coa_census_block_groups as b
 on a.geo_id = b.geo_id
-group by a.geo_id, b.geom
-
-select count(a.*), route_numb, a.geo_id, b.geom 
-from bus_routes_ls as a
-join coa_census_block_groups as b
-on a.geo_id = b.geo_id
-group by route_numb, a.geo_id, b.geom
+where route_id is null
+group by a.geo_id, b.geom;
 
